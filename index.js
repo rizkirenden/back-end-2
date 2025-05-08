055c5882ba98b7c757305360e8f4fe0f1a1e305f
rizkirenden <rizkirenden@gmail.com>
2025-05-08 23:34:13 +0800

    method POST

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const Data = "testing";
-console.log(Data);
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// routes utama method GET
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// routes method POST
+app.post("/login", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
